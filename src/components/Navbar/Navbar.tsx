46e08cb2232f858b8f3bba1912e40e1aadf09b19
PrashantRayamajhi <prashanrayamajhi@gmail.com>
2021-02-04 11:19:19 +0545

    add user and products compoents

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import { Link } from 'react-router-dom'
-import { green } from '@material-ui/core/colors'
+
 
 export default function Navbar() {
   // state for responsive navbar
