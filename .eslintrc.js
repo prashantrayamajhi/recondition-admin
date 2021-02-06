dc061d3a55fca65e039bf4524882c6ace909489e
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-02-06 19:24:07 +0100

    fix lf

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,12 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
-    'eslint linebreak-style': ['off', 'always']
+    'linebreak-style': ['off', 'always']
   },
 }
