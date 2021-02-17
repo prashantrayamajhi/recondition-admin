d2f75fb3708af0e8e69d6385eebe2da6775f9da7
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-02-17 11:59:27 +0100

    Update design

--- src/components/Navbar/HomeNav.tsx
+++ src/components/Navbar/HomeNav.tsx
@@ -17,31 +17,35 @@ export default function HomeNav() {
 
   return (
     <>
-      <div className='upper-nav'>
-        <div className='nav-contact'>
-          <div className='nav-contact-item'>
-            <Smartphone className='icon' style={{ color: '#2196f3' }} /> <span>+977-9876543210</span>
+      <div className='upper-nav-wrapper'>
+        <div className='upper-nav'>
+          <div className='nav-contact'>
+            <div className='nav-contact-item'>
+              <Smartphone className='icon' style={{ color: '#2196f3' }} /> <span>+977-9876543210</span>
+            </div>
+            <div className='nav-contact-item'>
+              <Timer className='icon' style={{ color: '#2196f3' }} /> <span>Sun-Fri: 10:00-6:00</span>
+            </div>
           </div>
-          <div className='nav-contact-item'>
-            <Timer className='icon' style={{ color: '#2196f3' }} /> <span>Sun-Fri: 10:00-6:00</span>
+          <div className='nav-social'>
+            <Link className='link' to=''><Facebook style={{ color: 'white', fontSize: 22 }} /></Link>
+            <Link className='link' to=''><Instagram style={{ color: 'white', fontSize: 22 }} /></Link>
+            <Link className='link' to=''><Mail style={{ color: 'white', fontSize: 22 }} /></Link>
           </div>
-        </div>
-        <div className='nav-social'>
-          <Link className='link' to=''><Facebook style={{ color : 'white', fontSize: 22 }}  /></Link>
-          <Link className='link' to=''><Instagram style={{ color : 'white', fontSize: 22 }}  /></Link>
-          <Link className='link' to=''><Mail style={{ color : 'white', fontSize: 22 }}  /></Link>
         </div>
       </div>
-      <nav className='navbar'>
-        <div className='nav-brand'>
-          <h2>Logo</h2>
-        </div>
-        <div className='nav-list'>
-          <Link to='/' className='nav-item'>Home</Link>
-          <a href='/#about' className='nav-item'>About</a>
-          <a href='/#services' className='nav-item'>Services</a>
-          <Link to='/products' className='nav-item'>Products</Link>
-          <a href='/#contact' className='nav-item'>Contact</a>
+      <nav className='navbar-wrapper'>
+        <div className='navbar'>
+          <div className='nav-brand'>
+            <h2>Logo</h2>
+          </div>
+          <div className='nav-list'>
+            <Link to='/' className='nav-item'>Home</Link>
+            <a href='/#about' className='nav-item'>About</a>
+            <a href='/#services' className='nav-item'>Services</a>
+            <Link to='/products' className='nav-item'>Products</Link>
+            <a href='/#contact' className='nav-item'>Contact</a>
+          </div>
         </div>
       </nav>
     </>
